e63652114392535590b559932c7c6f28b811ec9f
Евгений <ugend84@yandex.ru>
2022-06-22 12:32:31 +0300

    Коммит с неполучившимися тестами

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -8,4 +8,5 @@ import java.util.Collection;
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
     Collection<Student> findByAgeBetween(int min, int max);
+    Student findStudentByName(String name);
 }
--- src/test/java/ru/hogwarts/school/SchoolApplicationMockTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationMockTests.java
@@ -1,30 +1,53 @@
 package ru.hogwarts.school;
 
-import org.json.JSONObject;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.hogwarts.school.controller.FacultyController;
 import ru.hogwarts.school.interfac.FacultyService;
+import ru.hogwarts.school.interfac.StudentService;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.AvatarRepository;
 import ru.hogwarts.school.repository.FacultyRepository;
+import ru.hogwarts.school.repository.StudentRepository;
 
-import java.util.Optional;
-
-import static org.hamcrest.Matchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+
+@SpringBootTest
+//@ExtendWith(MockitoExtension.class)
+ @WebMvcTest
 public class SchoolApplicationMockTests {
     @Autowired
     private MockMvc mock;
 
     @MockBean
     private FacultyRepository facultyRepository;
+
+    @MockBean
+    private StudentRepository studentRepository;
+
+    @MockBean
+    private AvatarRepository avatarRepository;
+
+    @MockBean
+    private StudentService studentService;
 
     @SpyBean
     private FacultyService facultyService;
@@ -34,16 +57,26 @@ public class SchoolApplicationMockTests {
 
     @Test
     public void testAddFaculty() throws Exception {
-        JSONObject facultyObject = new JSONObject();
+        JSONObject facultyObject= new JSONObject();
         String name = "aaaa";
         String color = "ffffff";
+        long id = 100;
         facultyObject.put("name",name);
         facultyObject.put("color",color);
         Faculty f = new Faculty();
         f.setId(100);
         f.setName(name);
         f.setColor(color);
-        when(facultyRepository.save(any(Faculty.class))).thenReturn(f);
-        when(facultyRepository.findById(any(Long.class))).thenReturn(Optional.of(f));
+        when(facultyRepository.save(f)).thenReturn(f);
+       // when(facultyRepository.findById(id)).thenReturn(Optional.of(f));
+        mock.perform(MockMvcRequestBuilders
+                .post("/faculty")
+                .content(facultyObject.toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).
+                andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.color").value(color));
     }
 }
